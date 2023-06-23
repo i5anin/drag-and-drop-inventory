11dcb78bdb9400f80ec8ce0fb4e2e6dba8f89476
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-06-23 21:21:18 +0300

    Update InventoryPage.vue

--- src/pages/InventoryPage.vue
+++ src/pages/InventoryPage.vue
@@ -3,21 +3,21 @@
     <InventoryInfo />
     <InventoryGrid />
     <InventoryFooter />
-    <TestTest />
+    <InventoryFunctions />
   </div>
 </template>
 <script>
 import InventoryInfo from '@/components/InventoryInfo.vue'
 import InventoryGrid from '@/components/InventoryGrid.vue'
 import InventoryFooter from '@/components/InventoryFooter.vue'
-import TestTest from '@/components/test/TestTest.vue'
+import InventoryFunctions from '@/components/test/InventoryFunctions.vue'
 
 export default {
   components: {
     InventoryInfo,
     InventoryGrid,
     InventoryFooter,
-    TestTest,
+    InventoryFunctions,
   },
 }
 </script>
