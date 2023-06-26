5c138452f8072b189112d90dd8c3d3daa49b2a5f
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-06-26 16:02:12 +0300

    Revert "t"
    This reverts commit e73b444c7b7b8d2ed4ee51429342428f66e508a1.

--- src/components/CustomFunctions/ItemCard.vue
+++ src/components/CustomFunctions/ItemCard.vue
@@ -27,8 +27,6 @@
 </template>
 
 <script>
-// import { hexToRGBA } from './colorUtils.ts'
-
 export default {
   props: {
     color: {
@@ -73,7 +71,25 @@ export default {
       this.$emit('click', this.index)
     },
     // Преобразование цвета в формат RGBA с заданным значением альфа-канала
-    // hexToRGBA, // Import the hexToRGBA function from colorUtils.ts
+    hexToRGBA(hex, alpha) {
+      if (hex === null) {
+        return // Обработка случая, когда значение цвета равно null
+      }
+      const shiftAmount = 50 // Значение сдвига
+
+      // Получить значения R, G и B из HEX
+      let r = parseInt(hex.slice(1, 3), 16)
+      let g = parseInt(hex.slice(3, 5), 16)
+      let b = parseInt(hex.slice(5, 7), 16)
+
+      // Применить сдвиг к каждому компоненту цвета
+      r = Math.max(0, Math.min(r + shiftAmount, 255))
+      g = Math.max(0, Math.min(g + shiftAmount, 255))
+      b = Math.max(0, Math.min(b + shiftAmount, 255))
+
+      // Вернуть цвет в формате RGBA с заданным значением альфа-канала
+      return `rgba(${r}, ${g}, ${b}, ${alpha})`
+    },
   },
   computed: {
     // Вычисление цвета с альфа-каналом для стиля glass-effect
