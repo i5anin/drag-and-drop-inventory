9207393575de1c530dc69337eeffa39af0b2b51f
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-06-27 19:48:12 +0300

    Update InventoryPage.vue

--- src/pages/InventoryPage.vue
+++ src/pages/InventoryPage.vue
@@ -1,15 +1,34 @@
 <template>
   <div class="center">
-    <InventoryInfo />
-    <InventoryFooter />
-    <InventoryPage />
+    <div class="content">
+      <InventoryInfo />
+      <InventoryFooter />
+      <InventoryPage />
+    </div>
   </div>
 </template>
+
+<script>
+import InventoryInfo from '@/components/InventoryInfo.vue'
+import InventoryFooter from '@/components/InventoryFooter.vue'
+import InventoryPage from '@/components/InventoryGrid/InventoryPage.vue'
+export default {
+  components: {
+    InventoryInfo,
+    InventoryFooter,
+    InventoryPage,
+  },
+}
+</script>
 
 <style>
 .center {
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+.content {
+  position: absolute;
 }
 </style>
